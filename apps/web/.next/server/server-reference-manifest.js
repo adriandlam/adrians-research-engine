e8f1ade530593852638d2d2d23de98dff95ea9f6
Adrian Lam <adrianlamdev@gmail.com>
2025-03-06 16:33:09 -0800

    chore(.gitignore): update git ignore

--- apps/web/.next/server/server-reference-manifest.js
+++ apps/web/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9a6OtDwFZO3JxcqCFTYD1+PLaVJnL3bpXsuSCra6hvo=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
--- apps/web/.next/static/chunks/pages/_app.js
+++
@@ -1,8 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/[root of the server]__2cfeaf21._.js",
-  "static/chunks/545c3_react-dom_1c85ba3a._.js",
-  "static/chunks/node_modules__pnpm_f84813a4._.js",
-  "static/chunks/[root of the server]__20ba2906._.js",
-  "static/chunks/apps_web_pages__app_5771e187._.js",
-  "static/chunks/apps_web_pages__app_2935ccc1._.js"
-])
